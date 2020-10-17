ff37623149470d574a4c3fce130d93c05f4ea884
Magdalea Majkowska <magdalena.majkowska1@gmail.com>
2020-10-17 13:14:46 +0200

    Changed Sconscript so now we can have all the *.o files in build/ dir, executable program in bin/ and source files in src/.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
+//File: main.cpp
+
+//Program: SDL_HelloWorld
+
 #include <iostream>
-int main(void){
+using std::cout;
+using std::endl;
 
-    std::cout<<"Hello world\n";
+//SDL stuff.
+#include "SDL.h"
+
+int main (int argc, char* args[]) {
+    cout << "Hello World!\n";
+    
+    // Initialize SDL
+    if (SDL_Init (SDL_INIT_VIDEO) < 0)
+    {
+        cout << "Couldn't initialize SDL: %s\n";
+        exit (1);
+    } else {
+        cout << "SDL initialized correctly!\n";
+    }
+    //Important, otherwise stdout.txt
+    //might not be written two.
+    atexit (SDL_Quit);
+    
+    SDL_Quit();
+
     return 0;
-
 }
